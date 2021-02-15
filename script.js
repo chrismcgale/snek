05ffc00adf8c14fd553de39fb8bdb20a1a3a5e4c
chrismcgale <56483395+chrismcgale@users.noreply.github.com>
2021-02-14 23:18:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -166,7 +166,7 @@ function gen_food()
 
 function drawFood()
 {
-      snakeboard_ctx.fillStyle = 'lightgreen;
+      snakeboard_ctx.fillStyle = 'lightgreen';
       snakeboard_ctx.strokestyle = 'darkgreen';
       snakeboard_ctx.fillRect(food_x, food_y, 10, 10);
       snakeboard_ctx.strokeRect(food_x, food_y, 10, 10);
